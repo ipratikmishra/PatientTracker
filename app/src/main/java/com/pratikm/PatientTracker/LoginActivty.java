d4ac5fcbc441e55f6ff5b7d2e6cccb0e89896bee
pratikmishra <mishra.pratik29@gmail.com>
2017-04-06 01:18:21 +0530

    Add Login Page

---
+++ app/src/main/java/com/pratikm/PatientTracker/LoginActivty.java
@@ -0,0 +1,13 @@
+package com.pratikm.PatientTracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
